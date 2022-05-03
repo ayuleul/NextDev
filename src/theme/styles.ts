db4790a32bba4486881bd7c71f659e49645f807c
Leul Ayalew <leulayalew1888@gmail.com>
2022-05-03 20:54:11 +0300

    common styles defined

---
+++ src/theme/styles.ts
@@ -0,0 +1,27 @@
+import {colors} from './colors';
+
+export const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  flexRow: {
+    flexDirection: 'row',
+  },
+  flexColumn: {
+    flexDirection: 'column',
+  },
+  flexCenter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  justifyCenter: {
+    justifyContent: 'center',
+  },
+  alignCenter: {
+    alignItems: 'center',
+  },
+  justifyBetween: {
+    justifyContent: 'space-between',
+  },
+};
